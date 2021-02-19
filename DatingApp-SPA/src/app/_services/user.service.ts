1665a7b4fec67b0dce36fc189c7ce4d77f0bf433
ELKADIRI Abdelouhab <abdelouhabe@advancio.com>
2021-02-19 08:54:25 +0000

    End of section 15

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -18,7 +18,7 @@ export class UserService {
     this.userParams = new UserParams();
   }
 
-  getUsers(page?, itemsPerPage?, userParams?): Observable<PaginatedResult<User[]>> {
+  getUsers(page?, itemsPerPage?, userParams?, likesParam?): Observable<PaginatedResult<User[]>> {
     const paginatedResult: PaginatedResult<User[]> = new PaginatedResult<User[]>();
     let params = new HttpParams();
 
@@ -34,6 +34,15 @@ export class UserService {
       params = params.append('maxAge', userParams.maxAge);
       params = params.append('gender', userParams.gender);
       params = params.append('orderBy', userParams.orderBy);
+    }
+
+    if (likesParam === 'Likers')
+    {
+      params = params.append('likers', 'true');
+    }
+    if (likesParam === 'Likees')
+    {
+      params = params.append('likees', 'true');
     }
 
     return this.http.get<User[]>(this.baseUrl + 'users', { observe: 'response', params})
@@ -71,5 +80,9 @@ export class UserService {
 
   setUserParams(params: UserParams) {
     this.userParams = params;
+  }
+
+  sendLike(id: number, receptientId: number) {
+    return this.http.post(this.baseUrl + 'users/' + id + '/like/' + receptientId, {});
   }
 }
